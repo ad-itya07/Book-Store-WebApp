708ae7ad59999f203b984b7f53ee3ea0dbaf9c02
ad-itya07 <jhaaditya2207@gmail.com>
2024-10-18 17:13:25 +0530

    updated for statehandling using react-redux

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,10 +2,13 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import { RouterProvider } from "react-router-dom";
 import { router } from "./routers/router";
+import store from "./redux/store";
+import { Provider } from "react-redux";
+import 'sweetalert2/dist/sweetalert2.js'
 import "./index.css";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
+  <Provider store={store}>
     <RouterProvider router={router} />
-  </StrictMode>
+  </Provider>
 );
