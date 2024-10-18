1d61b0b2389ac1abb16160fed63c278608e82b36
ad-itya07 <jhaaditya2207@gmail.com>
2024-10-18 22:06:56 +0530

    cart-page

---
+++ frontend/src/pages/cart/CartPage.jsx
@@ -0,0 +1,83 @@
+import React from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { Link } from "react-router-dom";
+import CartItem from "./cartItem";
+import { clearCart } from "../../redux/features/cart/cartSlice";
+
+const CartPage = () => {
+  const cartItems = useSelector((state) => state.cart.cartItems);
+  const subtotal = cartItems.reduce((total, item) => total + item.newPrice, 0);
+  const dispatch = useDispatch();
+  const handleClearCart = () => {
+    dispatch(clearCart())
+  }
+
+  return (
+    <>
+      <div className="flex mt-12 h-full flex-col overflow-hidden bg-white shadow-xl">
+        <div className="flex-1 overflow-y-auto px-4 py-6 sm:px-6">
+          <div className="flex items-start justify-between">
+            <div className="text-lg font-medium text-gray-900">
+              Shopping cart
+            </div>
+            <div className="ml-3 flex h-7 items-center ">
+              <button
+                type="button"
+                onClick={handleClearCart}
+                className="relative -m-2 py-1 px-2 bg-red-500 text-white rounded-md hover:bg-secondary transition-all duration-200  "
+              >
+                <span className="">Clear Cart</span>
+              </button>
+            </div>
+          </div>
+
+          <div className="mt-8">
+            <div className="flow-root">
+              {cartItems.length > 0 ? (
+                <ul role="list" className="-my-6 divide-y divide-gray-200">
+                  {cartItems.map((item) => (
+                    <CartItem key={item._id} Book={item} />
+                  ))}
+                </ul>
+              ) : (
+                <p>No product found!</p>
+              )}
+            </div>
+          </div>
+        </div>
+
+        <div className="border-t border-gray-200 px-4 py-6 sm:px-6">
+          <div className="flex justify-between text-base font-medium text-gray-900">
+            <p>Subtotal</p>
+            <p>${subtotal ? subtotal : 0.00}</p>
+          </div>
+          <p className="mt-0.5 text-sm text-gray-500">
+            Shipping and taxes calculated at checkout.
+          </p>
+          <div className="mt-6">
+            <Link
+              to="/checkout"
+              className="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700"
+            >
+              Checkout
+            </Link>
+          </div>
+          <div className="mt-6 flex justify-center text-center text-sm text-gray-500">
+            <Link to="/">
+              or
+              <button
+                type="button"
+                className="font-medium text-indigo-600 hover:text-indigo-500 ml-1"
+              >
+                Continue Shopping
+                <span aria-hidden="true"> &rarr;</span>
+              </button>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default CartPage;
---
+++ frontend/src/pages/cart/cartItem.jsx
@@ -0,0 +1,57 @@
+import React from "react";
+import { getImageUrl } from "../../utils/getImgUrl";
+import { Link } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { removeFromCart } from "../../redux/features/cart/cartSlice";
+
+const CartItem = ({Book}) => {
+    const dispatch = useDispatch();
+    
+    const handleRemoveItem= (product) => {
+        dispatch(removeFromCart(product))
+    }
+  return (
+    <div>
+      <li className="flex py-6">
+        <div className="h-24 w-24 flex-shrink-0 overflow-hidden rounded-md border border-gray-200">
+          <img
+            alt=""
+            src={`${getImageUrl(Book?.coverImage)}`}
+            className="h-full w-full object-cover object-center"
+          />
+        </div>
+
+        <div className="ml-4 flex flex-1 flex-col">
+          <div>
+            <div className="flex flex-wrap justify-between text-base font-medium text-gray-900">
+              <h3>
+                <Link to="/">{Book?.title}</Link>
+              </h3>
+              <p className="sm:ml-4">${Book?.newPrice}</p>
+            </div>
+            <p className="mt-1 text-sm text-gray-500 capitalize">
+              <strong>Category:</strong> {Book?.category}
+            </p>
+          </div>
+          <div className="flex flex-1 flex-wrap items-end justify-between space-y-2 text-sm">
+            <p className="text-gray-500">
+              <strong>Qty:</strong> 1
+            </p>
+
+            <div className="flex">
+              <button
+                onClick={() => handleRemoveItem(Book)}
+                type="button"
+                className="font-medium text-indigo-600 hover:text-indigo-500"
+              >
+                Remove
+              </button>
+            </div>
+          </div>
+        </div>
+      </li>
+    </div>
+  );
+};
+
+export default CartItem;
