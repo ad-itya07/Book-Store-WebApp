c413b89541e548eee49f943ae1f30004553c3beb
ad-itya07 <jhaaditya2207@gmail.com>
2024-10-18 17:14:04 +0530

    updated navbar with state handling of cart Item

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -7,6 +7,7 @@ import { LuShoppingCart } from "react-icons/lu";
 
 import { Link } from "react-router-dom";
 import avatarImg from "../assets/avatar.png";
+import { useSelector } from "react-redux";
 
 const navigation = [
   { name: "Dashboard", href: "/dashboard" },
@@ -17,8 +18,9 @@ const navigation = [
 
 const Navbar = () => {
   const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+  const cartItems = useSelector((state) => state.cart.cartItems);
 
-  const currentUser = true;
+  const currentUser = false;
 
   return (
     <header className="max-w-screen-2xl mx-auto px-4 py-6">
@@ -59,7 +61,10 @@ const Navbar = () => {
                   <div className="absolute right-0 mt-2 w-48 border border-gray-300 bg-white shadow-lg rounded-md z-40">
                     <ul className="py-2">
                       {navigation.map((item) => (
-                        <li key={item.name} onClick={() => setIsDropdownOpen(false)}>
+                        <li
+                          key={item.name}
+                          onClick={() => setIsDropdownOpen(false)}
+                        >
                           <Link
                             to={item.href}
                             className="block px-4 py-2 text-sm hover:bg-gray-100"
@@ -86,7 +91,13 @@ const Navbar = () => {
             className="bg-primary p-1 sm:px-6 py-2 flex items-center rounded-sm"
           >
             <LuShoppingCart />
-            <span>Basket</span>
+            {cartItems.length > 0 ? (
+              <span className="text-sm font-semibold sm:ml-1">
+                {cartItems.length}
+              </span>
+            ) : (
+              <span className="text-sm font-semibold sm:ml-1">0</span>
+            )}
           </Link>
         </div>
       </nav>
