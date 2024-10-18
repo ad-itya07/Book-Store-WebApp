abbca284dcd0cbaea2a46f0695a659134ea56aa7
ad-itya07 <jhaaditya2207@gmail.com>
2024-10-18 12:51:48 +0530

    navbar removed when on /login or /register

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,13 @@
-import { Outlet } from "react-router-dom";
+import { Outlet , useLocation } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 
 function App() {
+  const location = useLocation();
   return (
     <>
-      <Navbar />
+      {location.pathname !== "/login" && location.pathname !== "/register" && <Navbar />}
       <main className="min-h-screen max-w-screen-2xl mx-auto px-4 py-6 font-primary">
         <Outlet />
       </main>
