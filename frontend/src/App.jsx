76f74c42fbd6186c3fdd116e7998d7d1aa981e68
ad-itya07 <jhaaditya2207@gmail.com>
2024-10-19 01:37:28 +0530

    removed navbar for '/checkout' route

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,4 @@
-import { Outlet , useLocation } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
@@ -7,7 +7,9 @@ function App() {
   const location = useLocation();
   return (
     <>
-      {location.pathname !== "/login" && location.pathname !== "/register" && <Navbar />}
+      {location.pathname !== "/login" &&
+        location.pathname !== "/register" &&
+        location.pathname !== "/checkout" && <Navbar />}
       <main className="min-h-screen max-w-screen-2xl mx-auto px-4 py-6 font-primary">
         <Outlet />
       </main>
