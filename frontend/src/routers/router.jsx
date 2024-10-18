4bbfa0d6c4ea9e17131765f4590939ca04e43e11
ad-itya07 <jhaaditya2207@gmail.com>
2024-10-18 22:06:01 +0530

    added cart-page route

--- frontend/src/routers/router.jsx
+++ frontend/src/routers/router.jsx
@@ -3,6 +3,7 @@ import App from "../App.jsx";
 import Home from "../pages/home/Home.jsx";
 import Register from "../components/Register.jsx";
 import Login from "../components/Login.jsx";
+import CartPage from "../pages/cart/CartPage.jsx";
 
 export const router = createBrowserRouter([
   {
@@ -28,6 +29,10 @@ export const router = createBrowserRouter([
       {
         path: '/register',
         element: <Register />
+      },
+      {
+        path: '/cart',
+        element: <CartPage />
       }
     ]
   },
