65d5f6f571aeff8c043aede981b6152d338d30d7
ad-itya07 <jhaaditya2207@gmail.com>
2024-10-19 23:34:51 +0530

    book controller

---
+++ backend/src/controllers/bookController.js
@@ -0,0 +1,115 @@
+import { PrismaClient } from "@prisma/client";
+import cloudinary from "../config/cloudinaryconfig.js";
+import {createBook, deleteBook, getABook, getBooks } from "../models/bookModel.js";
+
+const prisma = new PrismaClient();
+
+class BookController {
+  async createBookController(req, res) {
+    let result;
+    try {
+      result = await cloudinary.uploader.upload(req.file.path, {
+        folder: "Book Covers",
+        public_id: `${req.body.title}-cover`,
+      });
+    } catch (err) {
+      console.error(err);
+      return res.status(500).json({
+        message: "Error uploading to cloudinary!",
+        success: false,
+        error: err,
+      });
+    }
+
+    const bookData = {
+      ...req.body,
+      coverImage: result.secure_url,
+    };
+    if (req.body.trending === "true") {
+      bookData.trending = true;
+    } else {
+      bookData.trending = false;
+    }
+
+    bookData.oldPrice = parseFloat(bookData.oldPrice);
+    bookData.newPrice = parseFloat(bookData.newPrice);
+    try {
+      const addedBook = await createBook(bookData);
+      res.status(200).json(addedBook);
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({
+        message: "Error adding book to DB",
+        success: false,
+        error: err,
+      });
+    }
+  }
+
+  async getAllBooksController(req, res) {
+    try {
+      const allBooks = await getBooks();
+      res.status(200).json(allBooks);
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({
+        message: "Error fetching books",
+        success: false,
+        error: err,
+      });
+    }
+  }
+
+  async getABookController(req, res) {
+    const id = parseInt(req.params.id);
+    try {
+      const reqBook = await getABook(id);
+      if (!reqBook) {
+        return res.status(404).json({
+          message: "No book found!",
+          success: false,
+        });
+      }
+      res.status(200).json(reqBook);
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({
+        message: "Error fetching book!",
+        success: false,
+        error: err,
+      });
+    }
+  }
+
+  async updateBookController(req, res) {
+    const newData = req.body;
+    res.json({ newData });
+  }
+
+  async deleteBookController(req, res) {
+    const id = parseInt(req.params.id);
+    const toDelete = await getABook(id);
+
+    if (!toDelete) {
+      return res.status(404).json({
+        message: "No book found",
+        success: false,
+      });
+    }
+    try {
+      const deletedBook = await deleteBook(id);
+      return res.status(200).json({
+        deletedBook,
+        message: "Book successfully deleted!",
+        success: "true",
+      });
+    } catch (err) {
+      return res.status(500).json({
+        message: "Error deleting book from DB.",
+        success: false,
+        error: err,
+      });
+    }
+  }
+}
+export default new BookController();
