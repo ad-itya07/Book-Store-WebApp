8245eeca92f8b74a0fd0f8bb8ef66ed0be42f080
ad-itya07 <jhaaditya2207@gmail.com>
2024-10-19 23:33:32 +0530

    added book-router , cors-config

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,20 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-// import cloudinary from "./src/config/cloudinaryconfig.js";
-// import multer from "multer";
-// import { upload } from "./src/middlewares/filehandlerMiddleware.js";
+import {router as bookRouter} from "./src/routes/bookRoute.js";
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(cors());
 app.use(express.json());
+app.use(cors({
+    origin: 'http://localhost:5173',
+    credentials: true,
+  }));
 
-app.get("/", async (req, res) => {
-  res.send("Hello World!");
-});
+app.use("/api/books", bookRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
