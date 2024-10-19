eb479fabef9015921dfa1fb7ea3b7b41e6e260a2
ad-itya07 <jhaaditya2207@gmail.com>
2024-10-19 23:35:21 +0530

    prisma-func-model for book

--- backend/src/models/bookModel.js
+++ backend/src/models/bookModel.js
@@ -3,20 +3,32 @@ import { PrismaClient } from "@prisma/client";
 const prisma = new PrismaClient();
 
 export const getBooks = async () => {
-  return prisma.book.findMany();
-}
+  return await prisma.book.findMany({
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+};
 
-export const getBookById = async (id) => {
-    return await prisma.book.findUnique({
-        where: {
-        id: parseInt(id)
-        }
-    });
-}
+export const getABook = async (id) => {
+  return await prisma.book.findUnique({
+    where: {
+      id: id,
+    },
+  });
+};
 
-export const createBook = async (data) => {
-    return await prisma.book.create({
-        data: data
-    })
-}
+export const createBook = async (book) => {
+  // console.log(book);
+  return await prisma.book.create({
+    data: book,
+  });
+};
 
+export const deleteBook = async (id) => {
+  return await prisma.book.delete({
+    where: {
+      id: id,
+    },
+  });
+};
