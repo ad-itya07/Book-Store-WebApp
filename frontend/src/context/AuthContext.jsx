9a58d43bdba8d6bc13babe181bd002d3dd8c664b
ad_itya07 <jhaaditya2207@gmail.com>
2024-10-23 11:23:44 +0530

    added login, google-signin, signout, manage-user

--- frontend/src/context/AuthContext.jsx
+++ frontend/src/context/AuthContext.jsx
@@ -1,34 +1,70 @@
-import {createContext, useContext, useState} from 'react'
-import {auth} from '../firebase/firebaseConfig.js'
-import {createUserWithEmailAndPassword} from 'firebase/auth'
+import { createContext, useContext, useEffect, useState } from "react";
+import { auth, googleProvider } from "../firebase/firebaseConfig.js";
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signInWithPopup,
+  signOut,
+} from "firebase/auth";
 
-const AuthContext = createContext()
+const AuthContext = createContext();
 
 export const useAuth = () => {
-    const user = useContext(AuthContext);
+  const user = useContext(AuthContext);
+  return user;
+};
 
-    // if (user === undefined) {
-    //     throw new Error("useAuthContext must be used with a authContext.")
-    // }
-    return user;
-}
+// Auth Provider
+export const AuthProvider = ({ children }) => {
+  const [currentUser, setCurrentUser] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
 
-export const AuthProvider = ({ children}) => {
-    const [currentUser , setCurrentUser] = useState(null)
-    const [isLoading , setIsLoading] = useState(true)
+  // Register User
+  const registerUser = async (email, password) => {
+    return await createUserWithEmailAndPassword(auth, email, password);
+  };
 
-    const registerUser = async (email,password) => {
-        return await createUserWithEmailAndPassword(auth , email, password)
-    }
+  // Login User
+  const loginUser = async (email, password) => {
+    return await signInWithEmailAndPassword(auth, email, password);
+  };
 
-    const value = {
-        currentUser,
-        registerUser,
-    }
-    
-    return (
-        <AuthContext.Provider value={value}>
-            {children}
-        </AuthContext.Provider>
-    )
-}
+  // Google sign in
+  const signInWithGoogle = async () => {
+    return await signInWithPopup(auth, googleProvider);
+  };
+
+  // Sign OUT the user:
+  const logout = () => {
+    return signOut(auth);
+  };
+
+  //   Manage user
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(auth, (user) => {
+      setCurrentUser(user);
+      setIsLoading(false);
+
+      if (user) {
+        const { email, displayName, photoURL } = user;
+        const userData = {
+          email,
+          username: displayName,
+          photo: photoURL,
+        };
+      }
+    });
+
+    return () => unsubscribe();
+  }, []);
+  const value = {
+    currentUser,
+    registerUser,
+    loginUser,
+    signInWithGoogle,
+    logout,
+  };
+
+  return <AuthContext.Provider value={value}>{children}</AuthContext.Provider>;
+};
