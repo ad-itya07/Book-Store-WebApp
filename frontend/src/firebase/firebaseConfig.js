59d16a982c7287fe435d35d631270fd6810c0dc5
ad_itya07 <jhaaditya2207@gmail.com>
2024-10-23 11:24:14 +0530

    minor changes

--- frontend/src/firebase/firebaseConfig.js
+++ frontend/src/firebase/firebaseConfig.js
@@ -1,6 +1,6 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-import { getAuth }from 'firebase/auth'
+import { getAuth, GoogleAuthProvider }from 'firebase/auth'
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_API_KEY,
@@ -14,5 +14,6 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 // const analytics = getAnalytics(app);
+export const googleProvider = new GoogleAuthProvider()
 
 export const auth = getAuth(app);
