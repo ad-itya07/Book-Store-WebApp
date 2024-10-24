f2179269e3429adb56dff55fa04725bf0ef4c792
ad_itya07 <jhaaditya2207@gmail.com>
2024-10-24 12:33:04 +0530

    order routes

---
+++ backend/src/routes/orderRoute.js
@@ -0,0 +1,9 @@
+import express from "express";
+import orderController from "../controllers/orderController.js";
+
+export const router = express.Router();
+
+router.post("/place-order", orderController.createOrderController);
+router.get("/order-details/:orderId",orderController.getOrderDetailsController);
+router.get("/order-details",orderController.getAllOrderController);
+router.get("/order-details/user/:email", orderController.getOrderOfUser)
