6ef34499386e235870809520864eebde2406873f
ad_itya07 <jhaaditya2207@gmail.com>
2024-10-25 20:57:07 +0530

    authContext and redux usage

--- frontend/src/pages/checkout/checkout.jsx
+++ frontend/src/pages/checkout/checkout.jsx
@@ -1,38 +1,59 @@
 import React, { useState } from "react";
 import { useForm } from "react-hook-form";
 import { useSelector } from "react-redux";
-import { Link } from "react-router-dom";
+import { Link, Navigate, useNavigate } from "react-router-dom";
+import { useAuth } from "../../context/AuthContext";
+import { useCreateOrderMutation } from "../../redux/features/orders/ordersApi";
 
 const Checkout = () => {
   const cartItems = useSelector((state) => state.cart.cartItems);
-  const subtotal = cartItems.reduce((total, item) => total + item.newPrice, 0).toFixed(2);
-    const currentUser = true; //TODO: get user from auth
+  const subtotal = cartItems
+    .reduce((total, item) => total + item.newPrice, 0)
+    .toFixed(2);
+  const { currentUser } = useAuth();
+
+  const navigate = useNavigate();
   const {
     register,
     handleSubmit,
     formState: { errors },
   } = useForm();
 
-  const onSubmit = (data) => {
-    console.log(data);
-    const newOrder = {
-        name: data.name,
-        email: currentUser?.email,
-        address: {
-            country: data.country,
-            state: data.state,
-            city: data.city,
-            address: data.address,
-            zipCode: data.zipCode,
-        },
-        phone: data.phone,
-        productIds: cartItems.map(item => item.id),
-        totalPrice: subtotal,
-    }
+  const [createOrder, isLoading] = useCreateOrderMutation();
+
+  const sanitizeString = (str) => str ? str.replace(/\0/g, '') : '';
+  const onSubmit = async (data) => {
+    const address = {
+      address: sanitizeString(data.address),
+      city: sanitizeString(data.city),
+      state: sanitizeString(data.state),
+      country: sanitizeString(data.country),
+      zipcode: sanitizeString(data.zipcode),
+    };
+
+    const order = {
+      name: sanitizeString(data.name),
+      email: sanitizeString(currentUser?.email),
+      phone: sanitizeString(data.phone),
+      totalPrice: parseFloat(subtotal),
+    };
+    console.log(order);
+    console.log(address);
+    console.log(cartItems);
+
+    const newOrder = { order, address, cartItems };
     console.log(newOrder);
+    try {
+      await createOrder(newOrder).unwrap();
+      alert("Order Placed Successfully!")
+      navigate("/orders")
+    } catch {
+      console.error("Error placing an order!");
+      alert("Failed to place an order!");
+    }
   };
 
-const [isChecked , setIsChecked] = useState(false)
+  const [isChecked, setIsChecked] = useState(false);
   return (
     <section>
       <div className="min-h-screen p-6 bg-gray-100 flex items-center justify-center">
@@ -61,23 +82,22 @@ const [isChecked , setIsChecked] = useState(false)
                     <div className="md:col-span-5">
                       <label htmlFor="full_name">Full Name</label>
                       <input
-                    //   {...register("name", {required: true})}
+                        {...register("name", { required: true })}
                         type="text"
-                        name="name"
+                        // name="name"
                         id="name"
                         className="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
                       />
                     </div>
 
                     <div className="md:col-span-5">
-                      <label htmlFor = "email">Email Address</label>
+                      <label htmlFor="email">Email Address</label>
                       <input
-                    //   {...register("email", {required: true})}
+                        {...register("email", { required: true })}
                         type="text"
-                        name="email"
+                        // name="email"
                         id="email"
                         className="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
-                        disabled
                         defaultValue={currentUser?.email}
                         placeholder="email@domain.com"
                       />
@@ -85,9 +105,9 @@ const [isChecked , setIsChecked] = useState(false)
                     <div className="md:col-span-5">
                       <label htmlFor="phone">Phone Number</label>
                       <input
-                    //   {...register("phone", {required: true})}
+                        {...register("phone", { required: true })}
                         type="number"
-                        name="phone"
+                        // name="phone"
                         id="phone"
                         className="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
                         placeholder="+123 456 7890"
@@ -97,9 +117,9 @@ const [isChecked , setIsChecked] = useState(false)
                     <div className="md:col-span-3">
                       <label htmlFor="address">Address / Street</label>
                       <input
-                    //   {...register("address", {required: true})}
+                        {...register("address", { required: true })}
                         type="text"
-                        name="address"
+                        // name="address"
                         id="address"
                         className="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
                         placeholder=""
@@ -109,9 +129,9 @@ const [isChecked , setIsChecked] = useState(false)
                     <div className="md:col-span-2">
                       <label htmlFor="city">City</label>
                       <input
-                    //   {...register("city", {required: true})}
+                        {...register("city", { required: true })}
                         type="text"
-                        name="city"
+                        // name="city"
                         id="city"
                         className="h-10 border mt-1 rounded px-4 w-full bg-gray-50"
                         placeholder=""
@@ -122,8 +142,8 @@ const [isChecked , setIsChecked] = useState(false)
                       <label htmlFor="country">Country / region</label>
                       <div className="h-10 bg-gray-50 flex border border-gray-200 rounded items-center mt-1">
                         <input
-                        // {...register("country", {required: true})}
-                          name="country"
+                          {...register("country", { required: true })}
+                          // name="country"
                           id="country"
                           placeholder="Country"
                           className="px-4 appearance-none outline-none text-gray-800 w-full bg-transparent"
@@ -168,9 +188,9 @@ const [isChecked , setIsChecked] = useState(false)
                       <label htmlFor="state">State / province</label>
                       <div className="h-10 bg-gray-50 flex border border-gray-200 rounded items-center mt-1">
                         <input
-                        // {...register("state", {required: true})}
-                          name="state"
+                          {...register("state", { required: true })}
                           id="state"
+                          // name="state"
                           placeholder="State"
                           className="px-4 appearance-none outline-none text-gray-800 w-full bg-transparent"
                         />
@@ -210,9 +230,9 @@ const [isChecked , setIsChecked] = useState(false)
                     <div className="md:col-span-1">
                       <label htmlFor="zipcode">Zipcode</label>
                       <input
-                    //   {...register("zipcode", {required: true})}
+                        {...register("zipcode", { required: true })}
                         type="text"
-                        name="zipcode"
+                        // name="zipcode"
                         id="zipcode"
                         className="transition-all flex items-center h-10 border mt-1 rounded px-4 w-full bg-gray-50"
                         placeholder=""
@@ -222,7 +242,7 @@ const [isChecked , setIsChecked] = useState(false)
                     <div className="md:col-span-5 mt-3">
                       <div className="inline-flex items-center">
                         <input
-                        onChange={() => setIsChecked(!isChecked)}
+                          onChange={() => setIsChecked(!isChecked)}
                           type="checkbox"
                           name="billing_same"
                           id="billing_same"
@@ -244,7 +264,7 @@ const [isChecked , setIsChecked] = useState(false)
                     <div className="md:col-span-5 text-right">
                       <div className="inline-flex items-end">
                         <button
-                            disabled={!isChecked}
+                          disabled={!isChecked}
                           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
                         >
                           Place an Order
