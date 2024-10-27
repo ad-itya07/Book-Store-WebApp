a6b533635264afa86a92512aa9c008e91f123ac4
ad_itya07 <jhaaditya2207@gmail.com>
2024-10-27 08:16:54 +0530

    enhanced user-system with conditional rendering

--- frontend/src/pages/cart/CartPage.jsx
+++ frontend/src/pages/cart/CartPage.jsx
@@ -6,12 +6,14 @@ import { clearCart } from "../../redux/features/cart/cartSlice";
 
 const CartPage = () => {
   const cartItems = useSelector((state) => state.cart.cartItems);
-  const subtotal = cartItems.reduce((total, item) => total + item.newPrice, 0).toFixed(2);
-  
+  const subtotal = cartItems
+    .reduce((total, item) => total + item.newPrice, 0)
+    .toFixed(2);
+
   const dispatch = useDispatch();
   const handleClearCart = () => {
-    dispatch(clearCart())
-  }
+    dispatch(clearCart());
+  };
 
   return (
     <>
@@ -21,15 +23,17 @@ const CartPage = () => {
             <div className="text-lg font-medium text-gray-900">
               Shopping cart
             </div>
-            <div className="ml-3 flex h-7 items-center ">
-              <button
-                type="button"
-                onClick={handleClearCart}
-                className="relative -m-2 py-1 px-2 bg-red-500 text-white rounded-md hover:bg-secondary transition-all duration-200  "
-              >
-                <span className="">Clear Cart</span>
-              </button>
-            </div>
+            {cartItems.length > 0 && (
+              <div className="ml-3 flex h-7 items-center ">
+                <button
+                  type="button"
+                  onClick={handleClearCart}
+                  className="relative -m-2 py-1 px-2 bg-red-500 text-white rounded-md hover:bg-secondary transition-all duration-200  "
+                >
+                  <span className="">Clear Cart</span>
+                </button>
+              </div>
+            )}
           </div>
 
           <div className="mt-8">
@@ -48,24 +52,29 @@ const CartPage = () => {
         </div>
 
         <div className="border-t border-gray-200 px-4 py-6 sm:px-6">
-          <div className="flex justify-between text-base font-medium text-gray-900">
-            <p>Subtotal</p>
-            <p>${subtotal ? subtotal : 0.00}</p>
-          </div>
-          <p className="mt-0.5 text-sm text-gray-500">
-            Shipping and taxes calculated at checkout.
-          </p>
-          <div className="mt-6">
+          {cartItems.length > 0 && (
+            <div>
+              <div className="flex justify-between text-base font-medium text-gray-900">
+                <p>Subtotal</p>
+                <p>${subtotal ? subtotal : 0.0}</p>
+              </div>
+              <p className="mt-0.5 text-sm text-gray-500">
+                Shipping and taxes calculated at checkout.
+              </p>
+            </div>
+          )}
+          {cartItems.length > 0 && (
             <Link
               to="/checkout"
               className="flex items-center justify-center rounded-md border border-transparent bg-indigo-600 px-6 py-3 text-base font-medium text-white shadow-sm hover:bg-indigo-700"
             >
               Checkout
             </Link>
-          </div>
+          )}
+          <div className="mt-6"></div>
           <div className="mt-6 flex justify-center text-center text-sm text-gray-500">
             <Link to="/">
-              or
+              {cartItems.length !== 0 && <span>or</span>}
               <button
                 type="button"
                 className="font-medium text-indigo-600 hover:text-indigo-500 ml-1"
