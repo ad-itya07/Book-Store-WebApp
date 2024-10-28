c72bc59bf7d67b64cdff0a49e6a2809f8f304009
ad_itya07 <jhaaditya2207@gmail.com>
2024-10-29 00:12:38 +0530

    added login controller

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -1,8 +1,12 @@
+import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
+import dotenv from "dotenv";
 import { createAdmin, getUser } from "../models/userModel.js";
 
+dotenv.config();
+
 class userController {
-  async createAdmin(req, res) {
+  async createAdminController(req, res) {
     const { username, password } = req.body;
 
     try {
@@ -48,12 +52,77 @@ class userController {
         admin,
       });
     } catch (err) {
-        console.error(err);
+      console.error(err);
       res.status(500).json({
-      message: "Signup failed!",
-      success: false,
-      error: err,
-    });
+        message: "Signup failed!",
+        success: false,
+        error: err,
+      });
+    }
+  }
+
+  async loginAdminController(req, res) {
+    const { username, password } = req.body;
+
+    try {
+      if (!username || !password) {
+        return res.status(400).json({
+          message: "please fill all fields",
+          success: false,
+        });
+      }
+
+      let admin;
+      try {
+        admin = await getUser(username);
+        if (!admin) {
+          return res.status(404).json({ error: "Incorrect username" });
+        }
+      } catch (err) {
+        console.error(err);
+        res.status(500).json({
+          message: "Failed to check username in DB!",
+          success: false,
+          error: err,
+        });
+      }
+
+      const isMatch = await bcrypt.compare(password, admin.password);
+      admin.password = null;
+      if (isMatch) {
+        jwt.sign(
+          { admin },
+          process.env.JWT_SECRET_KEY,
+          { expiresIn: "1m" },
+          (err, token) => {
+            if (err) {
+              return res.status(500).json({
+                message: "Token Generation Failed",
+                success: false,
+                err,
+              });
+            }
+            return res.json({ 
+                message: "Authentication Successfull!",
+                success: true,
+                token,
+                admin,
+             });
+          }
+        );
+      } else {
+        return res.status(400).json({
+          message: "Incorrect Password!",
+          success: false,
+        });
+      }
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({
+        message: "Signup failed!",
+        success: false,
+        error: err,
+      });
     }
   }
 }
