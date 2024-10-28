ef3b8667ffc3ce3c98d54fd035cfee31928b2574
ad_itya07 <jhaaditya2207@gmail.com>
2024-10-29 00:14:54 +0530

    updated admin-specific routes using JWT-verification-middleware

--- backend/src/routes/bookRoute.js
+++ backend/src/routes/bookRoute.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { upload } from "../middlewares/filehandlerMiddleware.js";
 import BookController from "../controllers/bookController.js";
+import { verifyAdminToken } from "../middlewares/jwtVerificationMiddleware.js";
 
 export const router = express.Router();
 
@@ -8,8 +9,8 @@ router.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-router.post("/create-book", upload.single("file"), BookController.createBookController);
+router.post("/create-book", verifyAdminToken, upload.single("file"), BookController.createBookController);
 router.get("/all-books", BookController.getAllBooksController);
 router.get("/:id", BookController.getABookController);
-router.put("/update-book/:id", BookController.updateBookController)
-router.delete("/delete-book/:id", BookController.deleteBookController)
+router.put("/update-book/:id", verifyAdminToken, BookController.updateBookController)
+router.delete("/delete-book/:id", verifyAdminToken, BookController.deleteBookController)
--- backend/src/routes/orderRoute.js
+++ backend/src/routes/orderRoute.js
@@ -1,10 +1,11 @@
 import express from "express";
 import orderController from "../controllers/orderController.js";
+import { verifyAdminToken } from "../middlewares/jwtVerificationMiddleware.js";
 
 export const router = express.Router();
 
 router.post("/place-order", orderController.createOrderController);
-router.get("/order-details/:orderId",orderController.getOrderDetailsController);
-router.get("/order-details",orderController.getAllOrderController);
+router.get("/admin/order-details/:orderId", verifyAdminToken, orderController.getOrderDetailsController);
+router.get("/admin/order-details/all", verifyAdminToken,orderController.getAllOrderController);
 router.get("/order-details/user/:email", orderController.getOrdersOfUser);
 router.get("/order-details/user/:email/:orderId", orderController.getOrderDetailsByEmailAndId);
