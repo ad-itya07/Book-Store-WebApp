73412e3101f6799f8cb27a7f630365dba5cb9072
ad_itya07 <jhaaditya2207@gmail.com>
2024-10-28 21:37:02 +0530

    user router

---
+++ backend/src/routes/userRoute.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import userController from '../controllers/userController.js';1
+
+export const router = express.Router();
+
+router.get('/', (req, res) => {
+  res.send('Hello World');
+});
+
+// signup-route
+router.post('/admin/new' , userController.createAdmin);
