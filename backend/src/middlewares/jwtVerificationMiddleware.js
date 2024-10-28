e77d433e7ed1a01efbd9845167b217128a168278
ad_itya07 <jhaaditya2207@gmail.com>
2024-10-29 00:13:04 +0530

    JWT verification middleware

---
+++ backend/src/middlewares/jwtVerificationMiddleware.js
@@ -0,0 +1,39 @@
+import dotenv from "dotenv";
+import jwt from "jsonwebtoken";
+
+dotenv.config();
+
+export function verifyAdminToken(req, res, next) {
+  const bearerHeader = req.headers["authorization"];
+
+  if (bearerHeader) {
+    const bearer = bearerHeader.split(" ");
+    const bearerToken = bearer[1];
+
+    if (!bearerToken) {
+      return res.status(400).json({
+        message: "Access Declined, no token provided!",
+        success: false,
+      });
+    }
+
+    req.token = bearerToken;
+    jwt.verify(req.token, process.env.JWT_SECRET_KEY, (err, admin) => {
+      if (err) {
+        return res.status(403).json({
+          message: "Invalid Credentials",
+          success: false,
+          err,
+        });
+      } else {
+        req.admin = admin;
+        next();
+      }
+    });
+  } else {
+    return res.status(400).json({
+      message: "Access Declined, no header provided!",
+      success: false,
+    });
+  }
+}
