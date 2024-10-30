39f979d96b322c9d8cc11ae93298470942315123
ad_itya07 <jhaaditya2207@gmail.com>
2024-10-30 16:44:27 +0530

    Used tokenExpiry from local-storage to expire session

--- frontend/src/routers/AdminRoute.jsx
+++ frontend/src/routers/AdminRoute.jsx
@@ -3,7 +3,12 @@ import { Navigate, Outlet } from 'react-router-dom';
 
 const AdminRoute = ({children}) => {
   const token = localStorage.getItem('token');
-  if(!token) {
+  const tokenExpiry = localStorage.getItem("tokenExpiry");
+  if (!token || (tokenExpiry && Date.now() > parseInt(tokenExpiry, 10))) {
+    localStorage.removeItem("token");
+    localStorage.removeItem("tokenExpiry");
+
+    alert("Session expired. Please log in again.");
     return <Navigate to="/admin/login"/>
   }
   return children ?  children : <Outlet/>;
