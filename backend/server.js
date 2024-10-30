eadc41debc5ec28e4681bffa981b554ec865dc0d
Aditya Jha <141293751+ad-itya07@users.noreply.github.com>
2024-10-30 17:21:51 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://book-store-frontend-0sv0.onrender.com',
     credentials: true,
   }));
 
